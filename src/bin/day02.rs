62a7579ea93621780b10fbde5455258246c9eb55
chr.m <chri.marios@gmail.com>
2024-12-05 18:32:50 +0200

    day02 part2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -4,21 +4,32 @@ const INPUT: &str = include_str!("../../input/day02.txt");
 
 fn main() {
     run_puzzle!("Part 1", || save_report(INPUT));
+    run_puzzle!("Part 2", || problem_dampener(INPUT));
 }
 
-fn save_report(s: &str) -> u64 {
+fn problem_dampener(s: &str) -> u64 {
     let reports = parse(s);
 
     reports
         .iter()
         .filter(|report| {
-            let increasing = report[1] > report[0];
-            report.windows(2).all(|pair| {
-                let diff = (pair[1] - pair[0]).abs();
-                (1..=3).contains(&diff) && (pair[1] > pair[0]) == increasing
-            })
+            is_safe(report) || (0..report.len()).any(|i| is_safe(&remove_one(report, i)))
         })
         .count() as u64
+}
+
+fn save_report(s: &str) -> u64 {
+    let reports = parse(s);
+
+    reports.iter().filter(|report| is_safe(report)).count() as u64
+}
+
+fn is_safe(report: &[i32]) -> bool {
+    let increasing = report[1] > report[0];
+    report.windows(2).all(|pair| {
+        let diff = (pair[1] - pair[0]).abs();
+        (1..=3).contains(&diff) && (pair[1] > pair[0]) == increasing
+    })
 }
 
 fn parse(s: &str) -> Vec<Vec<i32>> {
@@ -31,6 +42,14 @@ fn parse(s: &str) -> Vec<Vec<i32>> {
         .collect()
 }
 
+fn remove_one(report: &[i32], index: usize) -> Vec<i32> {
+    report
+        .iter()
+        .enumerate()
+        .filter_map(|(i, &x)| if i == index { None } else { Some(x) })
+        .collect()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -40,5 +59,10 @@ mod test {
     #[test]
     fn test_safe_reports() {
         assert_eq!(save_report(INPUT), 2);
+    }
+
+    #[test]
+    fn test_problem_dampener() {
+        assert_eq!(problem_dampener(INPUT), 4);
     }
 }
